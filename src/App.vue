a84b773f6308b829af14b42014b501284c8370bb
Estevão <hurryblank@gmail.com>
2023-01-07 10:47:20 -0300

    add responsive media query

--- src/App.vue
+++ src/App.vue
@@ -11,24 +11,28 @@
 #app {
   min-width: 100vh;
   min-height: 100vh;
-  background:
-    linear-gradient(rgba(98, 0, 196, 0.6), rgba(11, 47, 4, 0.6));
+  /* background:
+    linear-gradient(rgba(98, 0, 196, 0.6), rgba(11, 47, 4, 0.6)); */
   font-family: var(--font-family);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  font-size: 22px;
+
+  overflow: hidden;
 
 }
 
 .templateView {
-  min-height: 80vh;
+  min-height: 100vh;
+  min-width: 100vw;
 }
 
 .navbar {
   font-weight: 600;
   background-color: var(--primary-color);
   color: var(--text-color) !important;
+  font-size: 22px !important;
+
 }
 
 .nav-link {
@@ -48,6 +52,19 @@
   height: 1.35rem;
   margin: 0 15px 0 0px;
   filter: invert(29%) sepia(6%) saturate(3997%) hue-rotate(184deg) brightness(101%) contrast(88%);
+}
+
+@media (max-width:1526px) {
+  .navbar {
+    justify-items: center !important;
+    align-content: space-between !important;
+  }
+
+  .navbar ul {
+    display: none !important;
+
+
+  }
 }
 </style>
 
--- src/components/menuBar.vue
+++ src/components/menuBar.vue
@@ -1,25 +1,34 @@
 <template>
     <div class="navbar justify-content-center">
-        <img class='logo' src="@/assets/LOGOTIPO.png" alt="Logo da Vértice">
+        <div class="container-fluid">
+            <div class="col-2 col-md-3">
+                <img class='logo' src="@/assets/LOGOTIPO.png" alt="Logo da Vértice">
+            </div>
 
-        <ul class="nav">
-            <li class="nav-item">
+            <div class="col col-md-5">
+                <ul v-bind:class="{}" class="nav">
+                    <li class="nav-item">
 
-                <router-link class="nav-link active" aria-current="page" href="#" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Calculadora</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Quem somos?</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Consultar CNPJ</a>
-            </li>
-        </ul>
-        <a @click.prevent="changeIcon()" href="#"><img class="icon" src="@/assets/brightness-high-fill.svg"></a>
-        <a href="#"><img class='icon' src="@/assets/person-gear.svg"></a>
-
+                        <router-link class="nav-link active" aria-current="page" href="#" to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Calculadora</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quem somos?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Consultar CNPJ</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col col-md-2">
+                <a v-if="theme" @click.prevent="changeIcon()" href="#"><img class="icon"
+                        src="@/assets/brightness-high-fill.svg"></a>
+                <a v-else @click.prevent="changeIcon()" href="#"><img class="icon" src="@/assets/moon-fill.svg"></a>
+                <a href="#"><img class='icon' src="@/assets/person-gear.svg"></a>
+            </div>
+        </div>
 
     </div>
 </template>
@@ -29,11 +38,30 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
     name: "menuBar",
-
+    data() {
+        return {
+            display: false,
+            theme: true,
+        }
+    },
     methods: {
         changeIcon() {
-            console.log("ola");
+            let theme = true;
+            theme = this.theme ? this.theme = true : this.theme = false;
+        },
+        onResize() {
+            window.innerWidth < 1526 ? this.display = true : this.display = false;
         }
-    }
+    },
+    created() {
+        window.addEventListener("resize", this.onResize)
+    },
+
 });
 </script>
+
+
+
+<style scoped>
+
+</style>
